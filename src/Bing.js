e69dfb821d0734e3502251338f2c296013d3c6ea
jayeshcholkar <115981022+jayeshcholkar@users.noreply.github.com>
2023-02-10 20:12:43 +0530

    first comt

--- src/Bing.js
+++ src/Bing.js
@@ -4,9 +4,9 @@ import {useEffect} from 'react'
 function Bing() {
 
 useEffect(() => {
-    axios.get("http://api.mediastack.com/v1/news?access_key=c19240f9190c455530d05c595e101893&sources=cnn,bbc")
-    .then((reponse)=>{
-    console.log(reponse.data)
+    axios.get('https://gnews.io/api/v4/search?q=example&lang=en&country=us&max=10&apikey=9da348c2e4af82c075b369ba1c840b03')
+    .then((articles)=>{
+    console.log(articles)
     })
 }, []);
 
